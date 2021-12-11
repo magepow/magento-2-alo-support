3a9a154bc12646d587b4b414938957b8cf1f9a3b
Alex Dong <team.magiccart@gmail.com>
2021-12-11 13:56:22 +0700

    Update url_rewrite_distinct.sql

--- url_rewrite_distinct.sql
+++ url_rewrite_distinct.sql
@@ -1,3 +1,4 @@
+# Ugrade 2.3.3 to 2.4.3 error: Integrity constraint violation
 CREATE TABLE catalog_url_rewrite_product_category_temporary SELECT DISTINCT url_rewrite_id, category_id, product_id FROM catalog_url_rewrite_product_category;
 
 DROP TABLE catalog_url_rewrite_product_category;
